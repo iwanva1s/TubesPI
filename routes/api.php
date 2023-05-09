a8761fac26f7e4f8f69d54a9474336da024b431e
janeanabel22 <janeanabel@students.usu.ac.id>
2023-05-09 20:46:16 +0700

    login and register

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\AuthController;
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    });
